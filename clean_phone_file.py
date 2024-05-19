ac6063e39034c307b7390476f9a13501aabbe815
jj2016025j <103946700+jj2016025j@users.noreply.github.com>
2024-05-20 00:07:22 +0800

    整理

--- clean_phone_file.py
+++
@@ -1,3 +0,0 @@
-target = [
-    '本機\OPPO Reno8 5G\內部共用儲存空間\Movies\Instagram'
-]
--- live_reload.py
+++
@@ -1,5 +0,0 @@
-from livereload import Server, shell
-
-server = Server()
-server.watch('*.py', shell('python file.py', cwd='.'))
-server.serve()
